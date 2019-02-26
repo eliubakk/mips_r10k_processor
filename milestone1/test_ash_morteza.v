d31f91cb77615642cc7beeed1b218ccea3c2b803
Ashirvad Varma <ashirvad.varma@gmail.com>
2019-02-26 11:43:57 -0500

    maybe fix

--- milestone1/test_ash_morteza.v
+++ milestone1/test_ash_morteza.v
@@ -407,6 +407,8 @@ module testbench;
 	inst_in.busy = 1;
 	LSQ_busy = 2'b00;
 
+
+	@(negedge clock);
 	$display("testing dispatch...");
 	entry_exists_in_table(inst_in, rs_table_out);
 	$display("entry exists in table!");
@@ -419,25 +421,31 @@ module testbench;
 	assert( !rs_full ) else #1 exit_on_error;
 	$display("Dispatch 1 instruction passed");
 
-	@(negedge clock);
 	$display("-------Issue One Instruction------\n");
 	// issue add p2 p1 p3
 	dispatch_valid = 0;
-
-	entry_not_in_table(inst_in, rs_table_out);
-	table_has_N_entries(0, rs_table_out);
 	assert( issue_next[0] == inst_in ) else #1 exit_on_error;
 	assert( issue_next[`NUM_FU-1:1] == issue_next_test[`NUM_FU-1:1] );
 	assert( issue_cnt == 1) else #1 exit_on_error;
 	assert( !rs_full ) else #1 exit_on_error;
-	$display("Issue 1 instruction passed");
 
 	@(negedge clock);
+
+	entry_not_in_table(inst_in, rs_table_out);
+	table_has_N_entries(0, rs_table_out);
+
+	$display("Issue 1 instruction passed");
+
+	assert( issue_cnt == 0) else #1 exit_on_error;
+	assert( !rs_full ) else #1 exit_on_error;
+
 	@(negedge clock);
 	$display("-------Commit One Instruction------\n");
 	// commit add p2 p1 p3
 	CAM_en = 1;
 	CDB_in = {1, 6'd3};
+
+	@(negedge clock);
 
 	tags_now_ready(3, rs_table_out);
 	table_has_N_entries(0, rs_table_out);
@@ -446,7 +454,6 @@ module testbench;
 	assert( !rs_full ) else #1 exit_on_error;
 	$display("Commit 1 Instruction passed");
 
-	@(negedge clock);
 	$display("-------Issue Multiple Instructions------\n");
 	// dispatch mult p2 p3 p4
 	enable = 1;
@@ -456,16 +463,20 @@ module testbench;
 	inst_in.T1 = {1, 6'd2};
 	inst_in.T2 = {1, 6'd3};
 	inst_in.busy = 1;
-	entry_exists_in_table(inst_in, rs_table_out);
-	table_has_N_entries(1, rs_table_out);
+
 	assert( issue_next == issue_next_test ) else #1 exit_on_error;
 	assert( !issue_cnt) else #1 exit_on_error;
-	assert( !rs_full ) else #1 exit_on_error;
 
 	@(negedge clock);
+
+	entry_exists_in_table(inst_in, rs_table_out);
+	table_has_N_entries(1, rs_table_out);
+
+	assert( !rs_full ) else #1 exit_on_error;
+
 	// check for issue for mult
-	entry_not_in_table(inst_in, rs_table_out);
-	table_has_N_entries(0, rs_table_out);
+	// entry_not_in_table(inst_in, rs_table_out);
+	// table_has_N_entries(0, rs_table_out);
 	assert( issue_next[0] == inst_in ) else #1 exit_on_error;
 	assert( issue_next[`NUM_FU-1:1] == issue_next_test[`NUM_FU-1:1] );
 	assert( issue_cnt == 1) else #1 exit_on_error;
@@ -477,13 +488,14 @@ module testbench;
 	inst_in.T1 = {0, 6'd4};
 	inst_in.T2 = {1, 6'd5};
 	inst_in.busy = 1;	
-	entry_exists_in_table(inst_in, rs_table_out);
-	table_has_N_entries(1, rs_table_out);
+
 	assert( issue_next == issue_next_test ) else #1 exit_on_error;
 	assert( !issue_cnt) else #1 exit_on_error;
 	assert( !rs_full ) else #1 exit_on_error;
 
 	@(negedge clock);
+	entry_exists_in_table(inst_in, rs_table_out);
+	table_has_N_entries(1, rs_table_out);
 	// check nothing is issued
 	assert( issue_next == issue_next_test ) else #1 exit_on_error;
 	assert( !issue_cnt) else #1 exit_on_error;
@@ -495,15 +507,16 @@ module testbench;
 	inst_in.T1 = {0, 6'd4};
 	inst_in.T2 = {0, 6'd4};
 	inst_in.busy = 1;	
-	entry_exists_in_table(inst_in, rs_table_out);
-	table_has_N_entries(2, rs_table_out);
+
 	assert( issue_next == issue_next_test ) else #1 exit_on_error;
 	assert( !issue_cnt) else #1 exit_on_error;
 	assert( !rs_full ) else #1 exit_on_error;
 
 	@(negedge clock);
-	dispatch_valid = 0;
+	entry_exists_in_table(inst_in, rs_table_out);
 	table_has_N_entries(2, rs_table_out);
+	dispatch_valid = 0;
+	// table_has_N_entries(2, rs_table_out);
 	assert( issue_next == issue_next_test ) else #1 exit_on_error;
 	assert( !issue_cnt) else #1 exit_on_error;
 	assert( !rs_full ) else #1 exit_on_error;
@@ -522,10 +535,6 @@ module testbench;
 	// commit mult p2 p3 p4
 	CAM_en = 1;
 	CDB_in = {1, 6'd4};
-	tags_now_ready(4, rs_table_out);
-
-	// check for 2 issued instructions
-	table_has_N_entries(0, rs_table_out);
 	inst_in.inst = '{ALU_OPA_IS_REGA, ALU_OPB_IS_REGB, DEST_IS_REGC, ALU_ADDQ, FU_ALU, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}; 
 	inst_in.T = {1, 6'd6};
 	inst_in.T1 = {0, 6'd4};
@@ -539,6 +548,14 @@ module testbench;
 	issue_next_test[1] = inst_in;
 
 	check_issue_next_correct(issue_next, issue_next_test);
+
+
+	@(negedge clock);
+	tags_now_ready(4, rs_table_out);
+
+	// check for 2 issued instructions
+	table_has_N_entries(0, rs_table_out);
+
 	assert( issue_next == issue_next_test ) else #1 exit_on_error;
 	assert( issue_cnt == 2) else #1 exit_on_error;
 	assert( !rs_full ) else #1 exit_on_error;
