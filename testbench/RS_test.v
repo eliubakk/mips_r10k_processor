cef6c12a359230be77b00942c8e3177374079086
Ashirvad Varma <ashirvad.varma@gmail.com>
2019-02-27 11:23:44 -0500

    hi

--- testbench/RS_test.v
+++ testbench/RS_test.v
@@ -387,7 +387,7 @@ module testbench;
 		dispatch_valid = 1;
 		check_issue_next_correct(issue_next, issue_next_test);
 		@(negedge clock);
-		dispatch_valid = 0;
+		dispatch_valid = 1'b0;
 		inst_in.busy = 1'b1;
 		
 		table_has_N_entries(1, rs_table_out);
@@ -395,7 +395,7 @@ module testbench;
 		issue_next_test[0] = inst_in;
 		check_issue_next_correct(issue_next, issue_next_test);
 
-		dispatch_valid = 1;
+		dispatch_valid = 1'b1;
 		inst_in.inst.opa_select = ALU_OPA_IS_MEM_DISP;
 		inst_in.inst.opb_select = ALU_OPB_IS_REGB;
 		inst_in.inst.dest_reg = DEST_IS_REGA;
@@ -420,6 +420,8 @@ module testbench;
 		table_out();
 
 		@(negedge clock);
+		$display("HI");
+		table_out();
 		table_has_N_entries(1, rs_table_out);
 
 		inst_in.busy = 1'b1;
