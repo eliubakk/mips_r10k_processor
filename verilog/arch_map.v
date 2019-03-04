7fcafdc4d39f741371f7fbc0cbad0bef4b1d4176
heewookim <heewoo9108@gmail.com>
2019-03-04 16:59:49 -0500

    CDB and arch_map table

--- verilog/arch_map.v
+++ verilog/arch_map.v
@@ -3,8 +3,41 @@ module Arch_Map_Table(
 	input 	reset,
 	input	enable,
 	input PHYS_REG	T_new_in, // Comes from ROB during Retire
+	input PHYS_REG	T_old_in, //What heewoo added. It is required to find which entry should I update. Comes from ROB during retire.
 
 	output GEN_REG [`NUM_GEN_REG-1:0] arch_table // Arch table status
 );
+
+	PHYS_REG		[`NUM_GEN_REG-1:0]	 	arch_map_table; 
+	logic			[$clog2(`NUM_GEN_REG)-1:0]		retire_idx; // general register index which physical register should be updated
+	logic			retire_hit;
+
+	assign arch_table = arch_map_table;
+
+	always_comb begin
+		retire_idx = {`NUM_GEN_REG{0}};
+		retire_hit = 1'b0;
+
+		if(enable) begin
+			for (integer i=0;i<`NUM_GEN_REG;i=i+1) begin
+				if( arch_map_table[i] == T_old_in ) begin
+					retire_idx = i;
+					retire_hit = 1'b1;
+				end
+			end
+		end	
+
+	end
+
+	always_ff @(posedge clock) begin
+		if(reset) begin
+			for(integer i=0;i<`NUM_GEN_REG;i=i+1) begin
+				arch_map_table[i] <=  i; //GEN_REG[i] = PHYS_REG[i]			
+			end
+		end else if (enable && retire_hit) begin
+				arch_map_table[retire_idx] <= T_new_in;	
+		end
+	
+	end
 
 endmodule // Arch_Map_Table
--- verilog/cdb.v
+++ verilog/cdb.v
@@ -4,11 +4,26 @@ module CDB (
 	input enable, // Clock Enable
 
 	input PHYS_REG tag_in,	// Comes from FU, during commit
-	input			ex_valid, // Comes from FU, during commit
+	input		ex_valid, // Comes from FU, during commit
 
 	output PHYS_REG CDB_tag_out, // Output for commit, goes to modules
-	output 			CDB_en_out,  // Output for commit, goes to modules
-	output busy
+	output 		CDB_en_out,  // Output for commit, goes to modules
+	output 		busy
 );
+
+	always_ff @(posedge clock) begin
+		if(reset) begin
+			CDB_tag_out	<= 7'b1111111; // Dummy register
+			CDB_en_out 	<= 1'b0;
+			busy 		<= 1'b0;	
+		end else if (enable) begin
+			if(ex_valid) begin
+				CDB_tag_out	<= tag_in;
+				CDB_en_out	<= 1'b1;
+				busy		<= 1'b1;
+			end	
+		end
+
+	end
 
 endmodule
