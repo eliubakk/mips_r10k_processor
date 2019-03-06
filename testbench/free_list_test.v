8d699fd154501288aa633b9ec9266222a2500f9c
Ashirvad Varma <ashirvad.varma@gmail.com>
2019-03-05 22:16:28 -0500

    Reset test case for free_list

--- testbench/free_list_test.v
+++ testbench/free_list_test.v
@@ -4,6 +4,12 @@
 `define CLOCK_PERIOD #10
 
 module testbench;
+
+	// parameters
+
+	parameter ONE = 1'b1;
+	parameter ZERO = 1'b0;
+
 	// initialize wires
 
 	// input wires
@@ -21,16 +27,29 @@ module testbench;
 	
 	// initialize module
 
-	Free_list(
+	`DUT(Free_List) fl0(
+		// inputs
 		.clock(clock),
 		.reset(reset),
 		.enable(enable),
 		.T_old(T_old),
 		.dispatch_en(dispatch_en);
+		// outputs
 		.num_free_entries(num_free_entries),
 		.empty(empty),
 		.free_reg(free_reg)
 	);
+
+
+	// TASKS
+	task exit_on_error;
+		begin
+			#1;
+			$display("@@@Failed at time %f", $time);
+			$finish;
+		end
+	endtask
+
 
 	// set clock change
 	always `CLOCK_PERIOD clock = ~clock;
@@ -38,8 +57,29 @@ module testbench;
 	initial begin
 
 		// monitor wires
-		$monitor();
+		$monitor("clock: %b reset: %b enable: %b T_old: %d dispatch_en: %b num_free_entries: %d empty: %b free_reg: %d", 
+				clock, reset, enable, T_old, dispatch_en, num_free_entries, empty, free_reg);
 
+		// intial values
+		clock = ZERO;
+		reset = ZERO;
+		enable = ZERO;
+		T_old =  0;
+		dispatch_en = ZERO;
+
+		$display("Testing Reset...");
+		@(negedge clock);
+		reset = ONE;
+
+		@(posedge clock);
+		`DELAY;
+		assert(num_free_entries == `NUM_PHYS_REG) else #1 exit_on_error;
+		assert(!empty) else #1 exit_on_error;
+
+		$display("Reset Test Passed");
+
+		$display("ALL TESTS Passed");
+		$finish;
 	end
 
 
