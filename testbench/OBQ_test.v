6c01c381d9da6419d18180b0c4d18c626668ce3e
Ashirvad Varma <ashvarma@umich.edu>
2019-03-19 19:47:33 -0400

    updated OBQ module with testbench, still not passing combination of shift and write

--- testbench/OBQ_test.v
+++ testbench/OBQ_test.v
@@ -18,17 +18,21 @@ module testbench;
 	logic write_en;
 	OBQ_ROW_T bh_row;
 	logic clear_en;
-	logic [$clog2(`OBQ_SIZE)-1:0] index;
+	logic [$clog2(`OBQ_SIZE):0] index;
+	logic shift_en;
+	logic [$clog2(`OBQ_SIZE):0] shift_index;
 
 	// output wires
 	OBQ_ROW_T [`OBQ_SIZE-1:0] obq_out;
 	logic [$clog2(`OBQ_SIZE)-1:0] tail_out;
+	logic [$clog2(`OBQ_SIZE):0] row_tag;
 	logic bh_pred_valid;
 	OBQ_ROW_T bh_pred;
 
 	// test wires
 	OBQ_ROW_T [`OBQ_SIZE-1:0] obq_test;
 	logic [$clog2(`OBQ_SIZE)-1:0] tail_test;
+	logic [$clog2(`OBQ_SIZE):0] row_tag_test;
 	
 	// initialize module
 	`DUT(OBQ) obq0(
@@ -39,10 +43,13 @@ module testbench;
 		.bh_row(bh_row),
 		.clear_en(clear_en),
 		.index(index),
+		.shift_en(shift_en),
+		.shift_index(shift_index),
 
 		// outputs
 		.obq_out(obq_out),
 		.tail_out(tail_out),
+		.row_tag(row_tag),
 		.bh_pred_valid(bh_pred_valid),
 		.bh_pred(bh_pred)
 	);
@@ -59,12 +66,58 @@ module testbench;
 	task obq_equal;
 		input OBQ_ROW_T [`OBQ_SIZE-1:0] obq;
 		input OBQ_ROW_T [`OBQ_SIZE-1:0] test;
-		input [$clog2(`OBQ_SIZE)-1:0] tail;
-		input [$clog2(`OBQ_SIZE)-1:0] tail_test;
+		input [$clog2(`OBQ_SIZE):0] tail;
+		input [$clog2(`OBQ_SIZE):0] tail_test;
 		begin
 			assert(tail == tail_test) else #1 exit_on_error;
 			for (int i = 0; i < tail; ++i) begin
 				assert(obq[i] == test[i]) else #1 exit_on_error;
+			end
+		end
+	endtask
+
+	task print_obq;
+		input OBQ_ROW_T [`OBQ_SIZE-1:0] obq;
+		begin
+			for (int i = 0; i < `OBQ_SIZE; ++i) begin
+				$display("i = %d bht: %b", i, obq[i].branch_history);
+			end
+		end
+	endtask
+
+	task insert_into_test;
+		input OBQ_ROW_T row;
+		begin
+			if (tail_test < `OBQ_SIZE) begin
+				obq_test[tail_test] = row;
+				row_tag_test = tail_test;
+				++tail_test;
+			end
+		end
+	endtask
+
+	task clear_from_test;
+		input int idx;
+		begin
+			if (idx < tail_test) begin
+				tail_test = idx;
+				obq_test[idx - 1].branch_history[`BH_SIZE - 1] = ~obq_test[idx - 1].branch_history[`BH_SIZE - 1];
+			end
+		end
+	endtask
+
+	task shift_test;
+		input int idx;
+		begin
+			$display("here tail_test: %d", tail_test);
+			if (idx < tail_test) begin
+				int count;
+				count = 0;
+				for (int i = idx + 1; i < `OBQ_SIZE; ++i) begin
+					obq_test[count] = obq_test[i];
+					++count;
+				end
+				tail_test -= idx;
 			end
 		end
 	endtask
@@ -75,8 +128,8 @@ module testbench;
 	initial begin
 
 		// monitor wires
-		$monitor("clock: %b reset: %b write_en: %b bh_row: %b clear_en: %b index: %d bh_pred_valid: %b bh_pred: %b",
-				clock, reset, write_en, bh_row, clear_en, index, bh_pred_valid, bh_pred);
+		$monitor("clock: %b reset: %b write_en: %b bh_row: %b clear_en: %b index: %d shift_en: %b shift_index: %d bh_pred_valid: %b bh_pred: %b",
+				clock, reset, write_en, bh_row, clear_en, index, shift_en, shift_index, bh_pred_valid, bh_pred);
 
 		// initialize
 		clock = ZERO;
@@ -85,13 +138,15 @@ module testbench;
 		bh_row.branch_history = 0;
 		clear_en = ZERO;
 		index = 0;
+		shift_en = ZERO;
+		shift_index = 0;
 
 		$display("Testing Reset...");
 		@(negedge clock);
 		reset = ONE;
 
 		for (int i = 0; i < `OBQ_SIZE; ++i) begin
-			obq_test[i].branch_history = 0;
+			obq_test[i].branch_history = 0; 
 		end
 		tail_test = 0;
 
@@ -100,6 +155,251 @@ module testbench;
 		obq_equal(obq_out, obq_test, tail_out, tail_test);
 
 		$display("Reset Test Passed");
+
+		$display("Testing Single Write...");
+
+		@(negedge clock);
+		reset = ZERO;
+		bh_row.branch_history = `BH_SIZE'b1010101011;
+		write_en = ONE;
+		insert_into_test(bh_row);
+
+		@(posedge clock);
+		`DELAY;
+		assert(bh_pred_valid == ONE) else #1 exit_on_error;
+		assert(bh_pred == bh_row) else #1 exit_on_error;
+		assert(row_tag == row_tag_test) else #1 exit_on_error;
+		obq_equal(obq_out, obq_test, tail_out, tail_test);
+
+		$display("Single Write Passed");
+
+		$display("Testing Multiple Write...");
+
+		for (int i = 1; i < 5; ++i) begin
+			@(negedge clock);
+			reset = ZERO;
+			bh_row.branch_history = $urandom_range(2**10,0);
+			write_en = ONE;
+			insert_into_test(bh_row);
+
+			@(posedge clock);
+			`DELAY;
+			assert(bh_pred_valid == ONE) else #1 exit_on_error;
+			assert(bh_pred == bh_row) else #1 exit_on_error;
+			assert(row_tag == row_tag_test) else #1 exit_on_error;
+			obq_equal(obq_out, obq_test, tail_out, tail_test);
+		end
+
+		$display("Multiple Write Passed");
+
+		$display("Testing Single Clear...");
+		@(negedge clock);
+		write_en = ZERO;
+		clear_en = ONE;
+		index = 4; // clear the last entry
+		clear_from_test(index);
+
+		@(posedge clock);
+		`DELAY;
+		assert(bh_pred_valid == ONE) else #1 exit_on_error;
+		assert(bh_pred == obq_test[3]) else #1 exit_on_error;
+		obq_equal(obq_out, obq_test, tail_out, tail_test);
+
+		$display("Single Clear Passed");
+
+		$display("Testing Multiple Clear...");
+
+		@(negedge clock);
+		clear_en = ONE;
+		index = 1;
+		clear_from_test(index);
+
+		@(posedge clock);
+		`DELAY;
+		assert(bh_pred_valid == ONE) else #1 exit_on_error;
+		assert(bh_pred == obq_test[0]) else #1 exit_on_error;
+		obq_equal(obq_out, obq_test, tail_out, tail_test);
+
+		$display("Multiple Clear Passed");
+
+		$display("Testing Clear All...");
+
+		// reset
+		@(negedge clock);
+		reset = ONE;
+		write_en = ZERO;
+		clear_en = ZERO;
+		tail_test = 0;
+
+		// insert
+		for (int i = 0; i < `OBQ_SIZE; ++i) begin
+			@(negedge clock);
+			reset = ZERO;
+			bh_row.branch_history = $urandom_range(2**10, 0);
+			write_en = ONE;
+			insert_into_test(bh_row);
+
+			@(posedge clock);
+			`DELAY;
+			assert(bh_pred_valid == ONE) else #1 exit_on_error;
+			assert(bh_pred == bh_row) else #1 exit_on_error;
+			assert(row_tag == row_tag_test) else #1 exit_on_error;
+			obq_equal(obq_out, obq_test, tail_out, tail_test);
+		end
+
+		// clear
+		@(negedge clock);
+		write_en = ZERO;
+		clear_en = ONE;
+		index = 0;
+		clear_from_test(index);
+
+		@(posedge clock);
+		`DELAY;
+		assert(bh_pred_valid == ZERO) else #1 exit_on_error;
+		obq_equal(obq_out, obq_test, tail_out, tail_test);
+		assert(tail_out == 0) else #1 exit_on_error;
+
+		$display("Clear All Passed");
+
+		$display("Testing Multiple Write and Multiple Clear...");
+
+		// reset
+		@(negedge clock);
+		reset = ONE;
+		write_en = ZERO;
+		clear_en = ZERO;
+
+		@(posedge clock);
+		`DELAY;
+		obq_test = obq_out;
+		tail_test = tail_out;
+
+		// random iterations
+		for (int i = 0; i < 100; ++i) begin
+			@(negedge clock);
+			reset = ZERO;
+			write_en = $urandom_range(1, 0);
+			clear_en = $urandom_range(1, 0);
+			bh_row.branch_history = $urandom_range(2**10 - 1, 0);
+			index = $urandom_range(2**4 - 1, 0);
+			if (clear_en) begin
+				clear_from_test(index);
+			end
+			if (write_en) begin
+				insert_into_test(bh_row);
+			end
+
+			@(posedge clock);
+			`DELAY;
+			assert(bh_pred_valid == (tail_test > 0)) else #1 exit_on_error;
+			obq_equal(obq_out, obq_test, tail_out, tail_test);
+			if (write_en) begin
+				assert(row_tag == row_tag_test) else #1 exit_on_error;
+			end
+		end
+
+		$display("Multiple Write and Multiple Clear Passed");
+
+		$display("Testing Single Shift...");
+
+		@(negedge clock);
+		reset = ONE;
+		write_en = ZERO;
+		clear_en = ZERO;
+
+		@(posedge clock);
+		`DELAY;
+		obq_test = obq_out;
+		tail_test = tail_out;
+
+		// insert random stuff
+		for (int i = 0; i < 6; ++i) begin
+			@(negedge clock);
+			reset = ZERO;
+			write_en = ONE;
+			bh_row.branch_history = $urandom_range(2**10 - 1, 0);
+			insert_into_test(bh_row);
+
+			@(posedge clock);
+			`DELAY;
+			assert(bh_pred_valid == (tail_test > 0)) else #1 exit_on_error;
+			obq_equal(obq_out, obq_test, tail_out, tail_test);
+			assert(row_tag == row_tag_test) else #1 exit_on_error;
+		end
+
+		@(negedge clock);
+		write_en = ZERO;
+		shift_en = ONE;
+		shift_index = 0;
+		shift_test(shift_index);
+
+		@(posedge clock);
+		`DELAY;
+		assert(bh_pred_valid == ONE) else #1 exit_on_error;
+		obq_equal(obq_out, obq_test, tail_out, tail_test);
+
+		$display("Single Shift Passed");
+
+		$display("Testing Multiple Shift...");
+
+		@(negedge clock);
+		write_en = ZERO;
+		shift_en = ONE;
+		shift_index = 4;
+		shift_test(shift_index);
+
+		@(posedge clock);
+		`DELAY;
+		assert(bh_pred_valid == ONE) else #1 exit_on_error;
+		obq_equal(obq_out, obq_test, tail_out, tail_test);
+
+		$display("Multiple Shift Passed");
+
+		$display("Testing Multiple Write and Multiple Shift...");
+
+		@(negedge clock);
+		shift_en = ZERO;
+		reset = ONE;
+
+		@(posedge clock);
+		`DELAY;
+		obq_test = obq_out;
+		tail_test = tail_out;
+
+		$display("at start tail_out: %d tail_test: %d", tail_out, tail_test);
+
+		for (int i = 0; i < 100; ++i) begin
+			@(negedge clock);
+			reset = ZERO;
+			write_en = $urandom_range(1, 0);
+			shift_en = $urandom_range(1, 0);
+			bh_row.branch_history = $urandom_range(2**10 - 1, 0);
+			shift_index = $urandom_range(2**4 - 1, 0);
+			$display("look here");
+			$display("tail_out: %d tail_test: %d", tail_out, tail_test);
+			print_obq(obq_out);
+			print_obq(obq_test);
+			if (shift_en) begin
+				shift_test(shift_index);
+			end
+			if (write_en) begin
+				insert_into_test(bh_row);
+			end
+
+			@(posedge clock);
+			`DELAY;
+			$display("tail_out: %d tail_test: %d", tail_out, tail_test);
+			print_obq(obq_out);
+			print_obq(obq_test);
+			assert(bh_pred_valid == (tail_out > 0)) else #1 exit_on_error;
+			obq_equal(obq_out, obq_test, tail_out, tail_test);
+			if (write_en) begin
+				assert(row_tag == row_tag_test) else #1 exit_on_error;
+			end
+		end
+
+		$display("Multiple Write and Multiple Shift Passed");
 
 		$display("ALL TESTS Passed");
 		$finish;
--- verilog/OBQ.v
+++ verilog/OBQ.v
@@ -1,7 +1,7 @@
-`include "sys_defs.vh"
+`include "../sys_defs.vh"
 `define DEBUG
 
-`define index_t ($clog2(`OBQ_SIZE) - 1)
+`define index_t ($clog2(`OBQ_SIZE))
 
 // in this module, we assume the oldest branch history table
 // is at the lowest index (i.e. index 0)
@@ -14,12 +14,16 @@ module OBQ(
 	input OBQ_ROW_T bh_row, // branch history row from pipeline
 	input clear_en,
 	input [`index_t:0] index,
+	input shift_en,
+	input [`index_t:0] shift_index,
 
 	// outputs
 	`ifdef DEBUG
 	output OBQ_ROW_T [`OBQ_SIZE-1:0] obq_out,
 	output [`index_t:0] tail_out,
 	`endif
+
+	output [`index_t:0] row_tag,
 	output bh_pred_valid,
 	output OBQ_ROW_T bh_pred // predicted branch history path
 );
@@ -31,55 +35,141 @@ module OBQ(
 	logic [`index_t:0] tail;
 	logic [`index_t:0] tail_next;
 
+	logic [`index_t:0] row_tag_index;
+	logic [`index_t:0] row_tag_next;
+
 	// assign statements
 	`ifdef DEBUG
 	assign obq_out = obq;
 	assign tail_out = tail;
 	`endif
 	assign bh_pred_valid = (tail > 0);
+	assign bh_pred = obq[tail-1];
+	assign row_tag = row_tag_index; // tail - 1;
 
+	// should refactor, this has a lot of if statements to avoid latches
 	// combinational next state logic
 	always_comb begin
-		if (write_en & clear_en) begin
+
+		if (write_en & clear_en & shift_en) begin
 			// we want to clear everything and then
 			// insert the new branch history table
-
-			// clear all the entries from counter onwards
+			
+			// shift all the entries over
 			for (int i = 0; i < `OBQ_SIZE; ++i) begin
-				if (i >= index) begin
-					obq_next[i].branch_history = 0;
-				end else begin
-					obq_next[i].branch_history = obq[i].branch_history;
+				if (i - shift_index > 0) begin
+					obq_next[i - shift_index - 1] = obq[i];
 				end
 			end
 
+			// clear the entries just by updating the tail ptr
+
+			if (index != 0) begin
+				// if we are clearing the table, this implies
+				// that our prediction was incorrect and
+				// therefore we need to update the last bit
+				// of the most recent valid branch history
+				obq_next[index - 1].branch_history[`BH_SIZE-1] = ~obq_next[index - 1].branch_history[`BH_SIZE-1];
+			end
+
 			// insert the new branch history
-			tail_next = index + 1;
-			obq_next[index] = bh_row;
+			tail_next = tail - shift_index - index + 1;
+			obq_next[tail - shift_index - index + 1] = bh_row;
+			row_tag_next = tail - shift_index - index + 1;
+		end else if (write_en & clear_en) begin
+			// clear everything and place new branch history at
+			// new end
+			if (index < tail) begin
+				tail_next = index + 1;
+				obq_next = obq;
+				obq_next[index] = bh_row;
+				row_tag_next = index;
+				if (index > 0) begin
+					// if we are clearing the table, this implies
+					// that our prediction was incorrect and
+					// therefore we need to update the last bit
+					// of the most recent valid branch history
+					obq_next[index - 1].branch_history[`BH_SIZE-1] = ~obq_next[index - 1].branch_history[`BH_SIZE-1];
+				end
+			end else begin
+				obq_next = obq;
+				obq_next[tail] = bh_row;
+				tail_next = tail + 1;
+				row_tag_next = tail;
+			end
+		end else if (write_en & shift_en) begin
+			// shift and append at end
+
+			// shift all the entries over
+			for (int i = 0; i < `OBQ_SIZE; ++i) begin
+				if (i - shift_index > 0) begin
+					obq_next[i - shift_index - 1] = obq[i];
+				end
+			end
+			tail_next = tail - shift_index + 1;
+
+			// insert new entry
+			obq_next[tail - shift_index] = bh_row;
+			row_tag_next = tail - shift_index;
+		end else if (clear_en & shift_en) begin
+			// shift all the entries over
+			for (int i = 0; i < `OBQ_SIZE; ++i) begin
+				if (i - shift_index > 0) begin
+					obq_next[i - shift_index - 1] = obq[i];
+				end
+			end
+
+			// clear the entries just by updating the tail ptr
+
+			if (index != 0) begin
+				// if we are clearing the table, this implies
+				// that our prediction was incorrect and
+				// therefore we need to update the last bit
+				// of the most recent valid branch history
+				obq_next[index - 1].branch_history[`BH_SIZE-1] = ~obq_next[index - 1].branch_history[`BH_SIZE-1];
+			end
+
+			tail_next = tail - shift_index - index;
+		end else if (shift_en) begin
+			// shift all the entries over
+			if (shift_index < tail) begin
+				for (int i = 0; i < `OBQ_SIZE; ++i) begin
+					if (i - shift_index > 0) begin
+						obq_next[i - shift_index - 1] = obq[i];
+					end
+				end
+				tail_next = tail - shift_index;
+			end else begin
+				tail_next = tail;
+				obq_next = obq;
+			end
 		end else if (write_en) begin
 			// write the newest branch history table into the 
 			// tail index of the obq
 			if (tail < `OBQ_SIZE) begin
+				obq_next = obq;
 				obq_next[tail] = bh_row;
 				tail_next = tail + 1;
+				row_tag_next = tail;
 			end else begin
 				obq_next = obq;
 				tail_next = tail;
+				row_tag_next = row_tag_index;
 			end
 		end else if (clear_en) begin
-
-			// clear all the entries from counter onwards
-			for (int i = 0; i < `OBQ_SIZE; ++i) begin
-				if (i >= index) begin
-					obq_next[i].branch_history = 0;
-				end else begin
-					obq_next[i].branch_history = obq[i].branch_history;
-				end
+			if (index < tail) begin
+				obq_next = obq;
+				obq_next[index - 1].branch_history[`BH_SIZE - 1] = ~obq[index - 1].branch_history[`BH_SIZE - 1];
+				tail_next = index;
+			end else begin
+				obq_next = obq;
+				tail_next = tail;
+				row_tag_next = row_tag_index;
 			end
-			tail_next = index;
 		end else begin
 			obq_next = obq;
 			tail_next = tail;
+			row_tag_next = row_tag_index;
 		end
 	end
 
@@ -90,9 +180,11 @@ module OBQ(
 				obq[i].branch_history <= 0; // reset to all not taken
 			end
 			tail <= 0;
+			row_tag_index <= 0;
 		end else begin
 			obq <= obq_next;
 			tail <= tail_next;
+			row_tag_index <= row_tag_next;
 		end
 	end
 
