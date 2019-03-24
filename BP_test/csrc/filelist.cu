4d0cf3791cb6c7959d5bede4a5f1b716bea77292
heewookim <heewoo9108@gmail.com>
2019-03-24 16:03:17 -0400

    BP.v not synthesized

--- BP_test/csrc/filelist.cu
+++ BP_test/csrc/filelist.cu
@@ -1,12 +1,12 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _1516_archive_1.so
-_1516_archive_1.so : archive.0/_1516_archive_1.a
+ARCHIVE_OBJS += _12591_archive_1.so
+_12591_archive_1.so : archive.0/_12591_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -o .//../simv.daidir//_1516_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -o .//../simv.daidir//_12591_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_1516_archive_1.so $@
+	@ln -sf .//../simv.daidir//_12591_archive_1.so $@
 
 
 
