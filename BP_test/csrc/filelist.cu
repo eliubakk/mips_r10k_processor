1ba080245c39995748fd56acb518162d2218c5ab
heewookim <heewoo9108@gmail.com>
2019-03-24 16:16:32 -0400

    BP.v synthesized

--- BP_test/csrc/filelist.cu
+++ BP_test/csrc/filelist.cu
@@ -1,12 +1,12 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _12591_archive_1.so
-_12591_archive_1.so : archive.0/_12591_archive_1.a
+ARCHIVE_OBJS += _9735_archive_1.so
+_9735_archive_1.so : archive.0/_9735_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -o .//../simv.daidir//_12591_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -o .//../syn_simv.daidir//_9735_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_12591_archive_1.so $@
+	@ln -sf .//../syn_simv.daidir//_9735_archive_1.so $@
 
 
 
@@ -16,7 +16,8 @@ _12591_archive_1.so : archive.0/_12591_archive_1.a
 %.o: %.c
 	$(CC_CG) $(CFLAGS_CG) -c -o $@ $<
 CU_UDP_OBJS = \
-
+objs/udps/hUcmi.o objs/udps/PjGxs.o objs/udps/dKp3B.o objs/udps/MzHq6.o objs/udps/GLrQJ.o  \
+objs/udps/guAtk.o objs/udps/F8ezs.o objs/udps/aKVa7.o 
 
 CU_LVL_OBJS = \
 SIM_l.o 
