bc3811b4277d598718639a24e7ac7e52a6448ba9
Ashirvad Varma <ashvarma@umich.edu>
2019-03-26 10:49:12 -0400

    remove comments of old code from Free_List_Check.v

--- verilog/Free_List_Check.v
+++ verilog/Free_List_Check.v
@@ -3,13 +3,6 @@
 
 module Free_List_Check(
 	input clock,
-// 	input reset,
-/*
-	input write_en,
-	input PHYS_REG T_old,
-	input clear_en,
-	input PHYS_REG T_new,
-*/
 	input enable,
 	input PHYS_REG [`FL_SIZE - 1:0] free_list_in,
 	input [$clog2(`FL_SIZE):0] tail_in,
@@ -38,15 +31,6 @@ module Free_List_Check(
 			free_list_next = free_list;
 			tail_next = tail;
 		end
-		/*
-		free_list_next = free_list;
-		tail_next = tail;
-
-		if (write_en & clear_en) begin
-		end else if (write_en) begin
-		end else if (clear_en) begin
-		end
-		*/
 	end
 
 	always_ff @(posedge clock) begin
@@ -54,17 +38,6 @@ module Free_List_Check(
 			free_list <= free_list_next;
 			tail <= tail_next;
 		end
-		/*
-		if (reset) begin
-			for (int i = 0; i < `NUM_GEN_REG; ++i) begin
-				free_list[i] <= {0, `NUM_GEN_REG + i};
-			end
-			tail <= `NUM_GEN_REG;
-		end else begin
-			free_list <= free_list_next;
-			tail <= tail_next;
-		end
-		*/
 	end
 
 endmodule
