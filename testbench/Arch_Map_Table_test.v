07b828a20075faefa67215caaa6784c74f12cd67
eliubakk <eliubakk@umich.edu>
2019-03-29 22:11:42 -0400

    Arch Map Table is now superscalar. Modified CAM to now output a hit signal for each input tag instead of or'ing them all together. This was useful for the arch map implementation.

--- testbench/Arch_Map_Table_test.v
+++ testbench/Arch_Map_Table_test.v
@@ -5,13 +5,14 @@
 `define DELAY #2
 
 module testbench;
-	logic clock, reset, enable;
-	PHYS_REG 					T_new_in;
-	PHYS_REG					T_old_in;
+	logic clock, reset;
+	logic [`SS_SIZE-1:0] 	enable;
+	PHYS_REG [`SS_SIZE-1:0] T_new_in;
+	PHYS_REG [`SS_SIZE-1:0] T_old_in;
 	
-	PHYS_REG		[`NUM_GEN_REG-1:0]	arch_table;
+	PHYS_REG [`NUM_GEN_REG-1:0]	arch_map_table;
 
-	integer					i,j,k;
+	integer i,j,k;
 
 	`DUT(Arch_Map_Table) AMT(
 		// inputs
@@ -21,7 +22,7 @@ module testbench;
 		.T_new_in(T_new_in), 
 		.T_old_in(T_old_in), 
 		// outputs 
-		.arch_table(arch_table)
+		.arch_map_table(arch_map_table)
 
 	 );
 
@@ -38,11 +39,11 @@ module testbench;
 		end
 	endtask
 
-	task display_arch_table;
+	task display_arch_map_table;
 		begin
 			$display("-----------Archtecture Map Table-----------");
-			for(k=0;k<`NUM_GEN_REG;k=k+1) begin
-				$display("Reg:%d, Phys Reg : %d", k, arch_table[k]); 
+			for(k = 0; k < `NUM_GEN_REG; k += 1) begin
+				$display("Reg:%d, Phys Reg : %d", k, arch_map_table[k][$clog2(`NUM_PHYS_REG)-1:0]); 
 			end
 			$display("------------------------------------------\n");	
 		end
@@ -50,15 +51,20 @@ module testbench;
 
 	
 	initial begin
-		
-		$monitor("Clock: %4.0f, reset: %b, enable:%b, T_new_in:%d, T_old_in:%d", clock, reset, enable, T_new_in, T_old_in);	
-
+		if(`SS_SIZE == 1) begin
+			$monitor("Clock: %4.0f, reset: %b, enable:%b, T_new_in:%d, T_old_in:%d", clock, reset, enable, T_new_in[0][$clog2(`NUM_PHYS_REG)-1:0], T_old_in[0][$clog2(`NUM_PHYS_REG)-1:0]);	
+		end else begin
+			$monitor("Clock: %4.0f, reset: %b, enable:%b\nT_new_in[2]:%d, T_old_in[2]:%d\nT_new_in[1]:%d, T_old_in[1]:%d\nT_new_in[0]:%d, T_old_in[0]:%d", clock, reset, enable, 
+									T_new_in[2][$clog2(`NUM_PHYS_REG)-1:0], T_old_in[2][$clog2(`NUM_PHYS_REG)-1:0], 
+									T_new_in[1][$clog2(`NUM_PHYS_REG)-1:0], T_old_in[1][$clog2(`NUM_PHYS_REG)-1:0], 
+									T_new_in[0][$clog2(`NUM_PHYS_REG)-1:0], T_old_in[0][$clog2(`NUM_PHYS_REG)-1:0]);	
+		end
 		// Initial value
 		clock = 1'b0;
 		reset = 1'b0;
-		enable = 1'b0;
-		T_new_in = 0;
-		T_old_in = 0;
+		enable = {`SS_SIZE{1'b0}};
+		T_new_in = {`SS_SIZE{`DUMMY_REG}};
+		T_old_in = {`SS_SIZE{`DUMMY_REG}};
 
 		@(negedge clock);
 		reset = 1'b1;
@@ -66,34 +72,35 @@ module testbench;
 		@(posedge clock);
 		`DELAY;
 		$display("-----After reset");
-		display_arch_table;
+		display_arch_map_table;
 
 		$display("-----Check the Arch Map functionality-----"); // Check the CDB functionality
 		
-		//
-		//
 		@(negedge clock);
 		reset = 1'b0;
-		enable = 1'b1;
-		T_old_in = 3;
-		T_new_in = 3 + `NUM_GEN_REG;
+		enable[`SS_SIZE-1] = 1'b1;
+		T_old_in[`SS_SIZE-1][$clog2(`NUM_PHYS_REG)-1:0] = 3;
+		T_old_in[`SS_SIZE-1][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_new_in[`SS_SIZE-1][$clog2(`NUM_PHYS_REG)-1:0] = (3 + `NUM_GEN_REG);
+		T_new_in[`SS_SIZE-1][$clog2(`NUM_PHYS_REG)] = 1'b1;
 		@(posedge clock);
 		`DELAY;
-		display_arch_table;
-		assert(arch_table[3]==3+`NUM_GEN_REG) else #1 exit_on_error;
+		display_arch_map_table;
+		assert(arch_map_table[3] == ((1'b1 << $clog2(`NUM_PHYS_REG)) | (3 + `NUM_GEN_REG))) else #1 exit_on_error;
 		
 		// Update phys reg to phys reg + 32	
-		for(i=0;i<`NUM_GEN_REG;i=i+1) begin
+		for(i = 0; i < `NUM_GEN_REG; i += 1) begin
 			@(negedge clock);
-			T_old_in = i;
-			T_new_in = i + `NUM_GEN_REG;
+			T_old_in[`SS_SIZE-1][$clog2(`NUM_PHYS_REG)-1:0] = i;
+			T_old_in[`SS_SIZE-1][$clog2(`NUM_PHYS_REG)] = 1'b1;
+			T_new_in[`SS_SIZE-1][$clog2(`NUM_PHYS_REG)-1:0] = (i + `NUM_GEN_REG);
+			T_new_in[`SS_SIZE-1][$clog2(`NUM_PHYS_REG)] = 1'b1;
 			@(posedge clock);
 			`DELAY;
-			display_arch_table;
-			assert(arch_table[i]==i+`NUM_GEN_REG) else #1 exit_on_error;
+			display_arch_map_table;
+			assert(arch_map_table[i] == ((1'b1 << $clog2(`NUM_PHYS_REG)) | (i + `NUM_GEN_REG))) else #1 exit_on_error;
 	
 		end
-
 
 		$display("-----Check reset-----"); // check reset
 		
@@ -101,49 +108,163 @@ module testbench;
 		reset = 1'b1;
 		@(posedge clock);
 		`DELAY;
-		for(j=0;j<`NUM_GEN_REG;j=j+1) begin
-			assert(arch_table[j]==j) else #1 exit_on_error;
+		enable = {`SS_SIZE{1'b0}};
+		reset = 1'b0;
+		for(i = 0; i < `NUM_GEN_REG; i += 1) begin
+			assert(arch_map_table[i] == ((1'b1 << $clog2(`NUM_PHYS_REG)) | i)) else #1 exit_on_error;
 
 		end
 
-		display_arch_table;
+		display_arch_map_table;
 	
 	
 		$display("-----Check enable-----");// check enable	
 		
 		@(negedge clock);
 		reset = 1'b0;
-		enable = 1'b0;
-		T_old_in = 7;
-		T_new_in = 7;
+		enable[`SS_SIZE-1] = 1'b0;
+		T_old_in[`SS_SIZE-1][$clog2(`NUM_PHYS_REG)-1:0] = 7;
+		T_old_in[`SS_SIZE-1][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_new_in[`SS_SIZE-1][$clog2(`NUM_PHYS_REG)-1:0] = (7 + `NUM_GEN_REG);
+		T_new_in[`SS_SIZE-1][$clog2(`NUM_PHYS_REG)] = 1'b1;
 		@(posedge clock);
 		`DELAY;
-		for(j=0;j<`NUM_GEN_REG;j=j+1) begin
-			assert(arch_table[j]==j) else #1 exit_on_error;
-
+		for(i = 0; i < `NUM_GEN_REG; i += 1) begin
+			assert(arch_map_table[i] == ((1'b1 << $clog2(`NUM_PHYS_REG)) | i)) else #1 exit_on_error;
 		end
 
-		display_arch_table;
+		display_arch_map_table;
 
 		// random numbers
 		$display("-----Random number check"); // check for random numbers
 
-		for(i=0;i<`NUM_GEN_REG;i=i+1) begin
+		for(i = 0; i < `NUM_GEN_REG; i += 1) begin
 			@(negedge clock);
-			enable = 1'b1;
-			T_old_in = i;
-			T_new_in = $urandom()%`NUM_GEN_REG + `NUM_GEN_REG;
+			enable[`SS_SIZE-1] = 1'b1;
+			T_old_in[`SS_SIZE-1][$clog2(`NUM_PHYS_REG)-1:0] = i;
+			T_old_in[`SS_SIZE-1][$clog2(`NUM_PHYS_REG)] = 1'b1;
+			T_new_in[`SS_SIZE-1][$clog2(`NUM_PHYS_REG)-1:0] = $urandom()%`NUM_GEN_REG + `NUM_GEN_REG;
+			T_new_in[`SS_SIZE-1][$clog2(`NUM_PHYS_REG)] = 1'b1;
 			@(posedge clock);
 			`DELAY;
-			display_arch_table;
-			assert(arch_table[i]==T_new_in) else #1 exit_on_error;
-	
+			display_arch_map_table;
+			assert(arch_map_table[i] == T_new_in[`SS_SIZE-1]) else #1 exit_on_error;
 		end
 
+		if(`SS_SIZE == 1) begin
+			$display("@@@passed");
+			$finish;	
+		end	
 
+		$display("------Test superscalar------");
+		
+		@(negedge clock);
+		reset = 1'b1;
+		@(posedge clock);
+		`DELAY;
+		enable = {`SS_SIZE{1'b0}};
+		reset = 1'b0;
+
+		//Three instructions with no dependencies
+		for(i = `SS_SIZE-1; i >= 0; i -= 1) begin
+			enable[i] = 1'b1;
+			T_old_in[i][$clog2(`NUM_PHYS_REG)-1:0] = (`SS_SIZE - 1 - i);
+			T_old_in[i][$clog2(`NUM_PHYS_REG)] = 1'b1;
+			T_new_in[i][$clog2(`NUM_PHYS_REG)-1:0] = ((`SS_SIZE - 1 - i) + `NUM_GEN_REG);
+			T_new_in[i][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		end
+		@(posedge clock);
+		`DELAY;
+		enable = {`SS_SIZE{1'b0}};
+		display_arch_map_table;
+		for(i = 0; i < `SS_SIZE; i += 1) begin
+			assert(arch_map_table[`SS_SIZE - 1 - i] == T_new_in[i]) else #1 exit_on_error;
+		end
+
+		//Three instructions dependency between 2 and 1
+		enable = {`SS_SIZE{1'b1}};
+		T_old_in[2][$clog2(`NUM_PHYS_REG)-1:0] = 3;
+		T_old_in[2][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_new_in[2][$clog2(`NUM_PHYS_REG)-1:0] = (3 + `NUM_GEN_REG);
+		T_new_in[2][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_old_in[1][$clog2(`NUM_PHYS_REG)-1:0] = (3 + `NUM_GEN_REG);
+		T_old_in[1][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_new_in[1][$clog2(`NUM_PHYS_REG)-1:0] = (4 + `NUM_GEN_REG);
+		T_new_in[1][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_old_in[0][$clog2(`NUM_PHYS_REG)-1:0] = 4;
+		T_old_in[0][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_new_in[0][$clog2(`NUM_PHYS_REG)-1:0] = (5 + `NUM_GEN_REG);
+		T_new_in[0][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		@(posedge clock);
+		`DELAY;
+		enable = {`SS_SIZE{1'b0}};
+		display_arch_map_table;
+		assert(arch_map_table[3] == T_new_in[1]) else #1 exit_on_error;
+		assert(arch_map_table[4] == T_new_in[0]) else #1 exit_on_error;
+
+		//Three instructions dependency between 2 and 0
+		enable = {`SS_SIZE{1'b1}};
+		T_old_in[2][$clog2(`NUM_PHYS_REG)-1:0] = 5;
+		T_old_in[2][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_new_in[2][$clog2(`NUM_PHYS_REG)-1:0] = (6 + `NUM_GEN_REG);
+		T_new_in[2][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_old_in[1][$clog2(`NUM_PHYS_REG)-1:0] = 6;
+		T_old_in[1][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_new_in[1][$clog2(`NUM_PHYS_REG)-1:0] = (8 + `NUM_GEN_REG);
+		T_new_in[1][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_old_in[0][$clog2(`NUM_PHYS_REG)-1:0] = (6 + `NUM_GEN_REG);
+		T_old_in[0][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_new_in[0][$clog2(`NUM_PHYS_REG)-1:0] = (7 + `NUM_GEN_REG);
+		T_new_in[0][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		@(posedge clock);
+		`DELAY;
+		enable = {`SS_SIZE{1'b0}};
+		display_arch_map_table;
+		assert(arch_map_table[5] == T_new_in[0]) else #1 exit_on_error;
+		assert(arch_map_table[6] == T_new_in[1]) else #1 exit_on_error;
+
+		//Three instructions dependency between 1 and 0
+		enable = {`SS_SIZE{1'b1}};
+		T_old_in[2][$clog2(`NUM_PHYS_REG)-1:0] = 7;
+		T_old_in[2][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_new_in[2][$clog2(`NUM_PHYS_REG)-1:0] = (9 + `NUM_GEN_REG);
+		T_new_in[2][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_old_in[1][$clog2(`NUM_PHYS_REG)-1:0] = 8;
+		T_old_in[1][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_new_in[1][$clog2(`NUM_PHYS_REG)-1:0] = (10 + `NUM_GEN_REG);
+		T_new_in[1][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_old_in[0][$clog2(`NUM_PHYS_REG)-1:0] = (10 + `NUM_GEN_REG);
+		T_old_in[0][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_new_in[0][$clog2(`NUM_PHYS_REG)-1:0] = (11 + `NUM_GEN_REG);
+		T_new_in[0][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		@(posedge clock);
+		`DELAY;
+		enable = {`SS_SIZE{1'b0}};
+		display_arch_map_table;
+		assert(arch_map_table[7] == T_new_in[2]) else #1 exit_on_error;
+		assert(arch_map_table[8] == T_new_in[0]) else #1 exit_on_error;
+
+		//Three instructions dependency between (2 and 1) and (1 and 0)
+		enable = {`SS_SIZE{1'b1}};
+		T_old_in[2][$clog2(`NUM_PHYS_REG)-1:0] = 9;
+		T_old_in[2][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_new_in[2][$clog2(`NUM_PHYS_REG)-1:0] = (12 + `NUM_GEN_REG);
+		T_new_in[2][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_old_in[1][$clog2(`NUM_PHYS_REG)-1:0] = (12 + `NUM_GEN_REG);
+		T_old_in[1][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_new_in[1][$clog2(`NUM_PHYS_REG)-1:0] = (13 + `NUM_GEN_REG);
+		T_new_in[1][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_old_in[0][$clog2(`NUM_PHYS_REG)-1:0] = (13 + `NUM_GEN_REG);
+		T_old_in[0][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		T_new_in[0][$clog2(`NUM_PHYS_REG)-1:0] = (14 + `NUM_GEN_REG);
+		T_new_in[0][$clog2(`NUM_PHYS_REG)] = 1'b1;
+		@(posedge clock);
+		`DELAY;
+		enable = {`SS_SIZE{1'b0}};
+		display_arch_map_table;
+		assert(arch_map_table[9] == T_new_in[0]) else #1 exit_on_error;
+		
 		$display("@@@passed");
-		$finish;		
-
-		end
-	
+		$finish;
+		end	
 endmodule
--- testbench/CAM_test.v
+++ testbench/CAM_test.v
@@ -6,26 +6,26 @@
 module testbench;
 	parameter LENGTH=16;
 	parameter WIDTH=2;
-	parameter NUM_TAG=3;
+	parameter NUM_TAGS=3;
 	parameter TAG_SIZE=6;
 	logic clock;
 	
 	//module inputs/outputs
-	logic [NUM_TAG-1:0] en;
-	logic [NUM_TAG-1:0][TAG_SIZE-1:0] tag;
-	logic [LENGTH-1:0][WIDTH-1:0][TAG_SIZE-1:0] tags_in;
+	logic [NUM_TAGS-1:0] en;
+	logic [NUM_TAGS-1:0][TAG_SIZE-1:0] tags;
+	logic [LENGTH-1:0][WIDTH-1:0][TAG_SIZE-1:0] table_in;
 	//outputs
-	logic [LENGTH-1:0][WIDTH-1:0] hits, hits_correct;
+	logic [LENGTH-1:0][WIDTH-1:0][NUM_TAGS-1:0] hits, hits_correct;
 
 	logic correct;
 
 	`DUT(CAM) #(.LENGTH(LENGTH),
 				.WIDTH(WIDTH),
-				.NUM_TAG(NUM_TAG),
+				.NUM_TAGS(NUM_TAGS),
 				.TAG_SIZE(TAG_SIZE)) CAM(
 		.enable(en), 
-		.tag(tag), 
-		.tags_in(tags_in), 
+		.tags(tags), 
+		.table_in(table_in), 
 		.hits(hits));
 
 	always #1 clock = ~clock;
@@ -44,11 +44,12 @@ module testbench;
 		string tag_str;
 		string hits_str;
 		string hit_bit;
+		string hit_str;
 
-		for(integer i = NUM_TAG - 1; i >= 0; i = i - 1) begin 
-			$display("tag: %b, en: %b", tag[i], en[i]);
+		for(integer i = NUM_TAGS - 1; i >= 0; i = i - 1) begin 
+			$display("tag: %b, en: %b", tags[i], en[i]);
 		end
-		$display("##### tags_in #####\t\t##### hits #####");
+		$display("##### table_in #####\t\t##### hits #####");
 
 		//logic [(TAG_SIZE*WIDTH + 2*(WIDTH-1) + WIDTH + 3):0] row_str;
 		for(integer i = LENGTH - 1; i >= 0; i = i - 1) begin
@@ -56,18 +57,22 @@ module testbench;
 			hits_str = "";
 			for(integer j = WIDTH - 1; j >= 0; j = j - 1) begin
 				tag_str = "";
-				hit_bit = "";
+				hit_str = "";
 				for(integer k = TAG_SIZE - 1; k >= 0; k = k - 1) begin
-					tag_bit.bintoa(tags_in[i][j][k]);
+					tag_bit.bintoa(table_in[i][j][k]);
 					tag_str = {tag_str, tag_bit};				
 				end
-				hit_bit.bintoa(hits[i][j]);
+				for(integer k = NUM_TAGS-1; k >= 0; k = k - 1) begin
+					hit_bit.bintoa(hits[i][j][k]);
+					hit_str = {hit_str, hit_bit};
+				end 
+				
 				if(j == (WIDTH - 1)) begin
 					tags_str = {tags_str, tag_str};
-					hits_str = {hits_str, hit_bit};
+					hits_str = {hits_str, hit_str};
 				end else begin
 					tags_str = {tags_str, " ", tag_str};
-					hits_str = {hits_str, " ", hit_bit};
+					hits_str = {hits_str, " ", hit_str};
 				end
 			end
 			$display({tags_str, "\t\t\t", hits_str});
@@ -85,11 +90,11 @@ module testbench;
 
 	task set_correct;
 		hits_correct = {(LENGTH*WIDTH){1'b0}};
-		for(integer k = NUM_TAG - 1; k >= 0; k = k - 1) begin
+		for(integer k = NUM_TAGS - 1; k >= 0; k = k - 1) begin
 			if(en[k]) begin
 				for(integer i = LENGTH - 1; i >= 0; i = i - 1) begin
 					for(integer j = WIDTH - 1; j >= 0; j = j - 1) begin
-						hits_correct[i][j] |= (tag[k] == tags_in[i][j]); 
+						hits_correct[i][j][k] = (tags[k] == table_in[i][j]);
 					end
 				end
 			end
@@ -101,14 +106,14 @@ module testbench;
 		$display("Starting test...");
 		//set starting values
 		clock = 1'b0;
-		en = {NUM_TAG{1'b0}};
-		tag = {TAG_SIZE*NUM_TAG{1'b1}};
-		tags_in = {WIDTH*LENGTH*TAG_SIZE{1'b0}};
+		en = {NUM_TAGS{1'b0}};
+		tags = {TAG_SIZE*NUM_TAGS{1'b1}};
+		table_in = {WIDTH*LENGTH*TAG_SIZE{1'b0}};
 		
 		//set all tags in a row to tag[0]
 		@(negedge clock);
 		for(integer i = WIDTH - 1; i >= 0; i = i - 1) begin
-			tags_in[0][i] = tag[0];
+			table_in[0][i] = tags[0];
 		end
 
 		//enable
@@ -117,43 +122,43 @@ module testbench;
 		
 		//set all tags in a column to tag
 		@(negedge clock);
-		tag[0] = {1'b1, {(TAG_SIZE-2){1'b0}}, 1'b1};
-		tags_in = {WIDTH*LENGTH*TAG_SIZE{1'b0}};
+		tags[0] = {1'b1, {(TAG_SIZE-2){1'b0}}, 1'b1};
+		table_in = {WIDTH*LENGTH*TAG_SIZE{1'b0}};
 		for(integer i = LENGTH - 1; i >= 0; i = i - 1) begin
-			tags_in[i][0] = tag[0];
+			table_in[i][0] = tags[0];
 		end
 
 		//set 2 random tags to each tag 
 		@(negedge clock);
-		tag[0] = {1'b0, {(TAG_SIZE-2){1'b1}}, 1'b0};
-		tag[1] = {{3{1'b1}}, {(TAG_SIZE-3){1'b0}}};
-		tag[2] = {TAG_SIZE{1'b1}};
+		tags[0] = {1'b0, {(TAG_SIZE-2){1'b1}}, 1'b0};
+		tags[1] = {{3{1'b1}}, {(TAG_SIZE-3){1'b0}}};
+		tags[2] = {TAG_SIZE{1'b1}};
 		//set all tags to random tags
 		for(integer i = LENGTH - 1; i >= 0; i = i - 1) begin
 			for(integer j = WIDTH - 1; j >= 0; j = j - 1) begin
-				tags_in[i][j] = $urandom_range(0,(2 ** TAG_SIZE) - 1);
+				table_in[i][j] = $urandom_range(0,(2 ** TAG_SIZE) - 1);
 			end
 		end
 		//set some random tags to the input tag
-		for(integer k = NUM_TAG - 1; k > 0; k = k - 1) begin
+		for(integer k = NUM_TAGS - 1; k > 0; k = k - 1) begin
 			for(integer i = 0; i < 2; i = i + 1) begin
 				integer row, column;
 				row = $urandom_range(0,LENGTH-1);
 				column = $urandom_range(0,WIDTH-1);
-				tags_in[row][column] = tag[k];
+				table_in[row][column] = tags[k];
 			end
 		end
-		en = {NUM_TAG{1'b1}};
+		en = {NUM_TAGS{1'b1}};
 
 		//set all tags randomly a few times
 		for(integer num = 0; num < 50; num = num + 1) begin
 			@(negedge clock);
-			for(integer k = NUM_TAG - 1; k > 0; k = k - 1) begin
-				tag[k] = $urandom_range(0,(2 ** TAG_SIZE) - 1);
+			for(integer k = NUM_TAGS - 1; k > 0; k = k - 1) begin
+				tags[k] = $urandom_range(0,(2 ** TAG_SIZE) - 1);
 			end
 			for(integer i = LENGTH - 1; i >= 0; i = i - 1) begin
 				for(integer j = WIDTH - 1; j >= 0; j = j - 1) begin
-					tags_in[i][j] = $urandom_range(0,(2 ** TAG_SIZE) - 1);
+					table_in[i][j] = $urandom_range(0,(2 ** TAG_SIZE) - 1);
 				end
 			end
 		end
