44d7eacceca32a45ca1c223dbaf6bc4873948786
Ashirvad Varma <ashvarma@umich.edu>
2019-03-30 15:24:01 -0400

    update the physical history table

--- verilog/PHT_TWO_SC.v
+++ verilog/PHT_TWO_SC.v
@@ -1,11 +1,13 @@
 // multiple row - 2 bit saturation counter - Pattern history table
 //
-`define DEBUG_OUT
-
+`define DEBUG
+`define PHT_ROW 8
+`timescale 1ns/100ps
 module PHT_TWO_SC(
 		input				clock,
 		input				reset,
 		input				enable,
+		input				if_branch,
 		input		[31:0]		if_pc_in,
 		input				if_cond_branch,
 		input				if_direct_branch,
@@ -17,7 +19,7 @@ module PHT_TWO_SC(
 		input				rt_return_branch,
 		input				rt_branch_taken, // Calculated branch result, 0 is not taken, 1 is taken
 
-		`ifdef DEBUG_OUT
+		`ifdef DEBUG
 		output	[`PHT_ROW-1:0][1:0]	pht_out,
 		`endif
 		output				if_prediction,
@@ -27,17 +29,23 @@ module PHT_TWO_SC(
 	// 00 : strongly not taken, 01 : weakly not taken, 10 : weakly taken,
 	// 11 : strongly taken 	
 
-	parameter	PHT_ROW		= 8;
 
 	logic 	[`PHT_ROW-1:0] [1:0] 	pht;
 	logic 	[`PHT_ROW-1:0] [1:0] 	next_pht;
-	logic	[$clog2(`PHT_ROW)-1:0]   pc_partial;
+	logic	[$clog2(`PHT_ROW)-1:0]   if_pc_partial;
+	logic	[$clog2(`PHT_ROW)-1:0]   rt_pc_partial;
 
-	`ifdef DEBUG_OUT
+	`ifdef DEBUG
 	assign pht_out 	= pht;
 	`endif	
 
-	assign pc_partial	=  pc_in [$clog2(`PHT_ROW)+1:2]; // do not consider the byte offset
+	assign if_pc_partial	=  if_pc_in [$clog2(`PHT_ROW)+1:2]; // do not consider the byte offset
+	assign if_prediction_valid = if_branch & if_cond_branch & !if_return_branch; 
+	assign if_prediction	=  if_prediction_valid? next_pht[if_pc_partial][1]: 0;
+				
+
+
+	assign rt_pc_partial	=  rt_pc_in [$clog2(`PHT_ROW)+1:2]; // do not consider the byte offset
 	//assign prediction	=  pht[pc_partial][1]; 
 	
 	always_comb begin
@@ -47,42 +55,37 @@ module PHT_TWO_SC(
 			// During retire, update the prediction table first
 			if(rt_branch & rt_cond_branch & !rt_return_branch) begin
 
-				case(pht[pc_partial][1:0])
+				case(pht[rt_pc_partial][1:0])
 					2'b00: begin
 							if(rt_branch_taken) begin
-								next_pht[pc_partial][1:0] = 2'b01;
+								next_pht[rt_pc_partial][1:0] = 2'b01;
 							end else begin
-								next_pht[pc_partial][1:0] = 2'b00;
+								next_pht[rt_pc_partial][1:0] = 2'b00;
 							end
 						end
 					2'b01: begin
 							if(rt_branch_taken) begin
-								next_pht[pc_partial][1:0] = 2'b10;
+								next_pht[rt_pc_partial][1:0] = 2'b10;
 							end else begin
-								next_pht[pc_partial][1:0] = 2'b00;
+								next_pht[rt_pc_partial][1:0] = 2'b00;
 							end
 	
 						end
 					2'b10: begin
 							if(rt_branch_taken) begin
-								next_pht[pc_partial][1:0] = 2'b11;
+								next_pht[rt_pc_partial][1:0] = 2'b11;
 							end else begin
-								next_pht[pc_partial][1:0] = 2'b01;
+								next_pht[rt_pc_partial][1:0] = 2'b01;
 							end
 						end
 					2'b11: begin
 							if(rt_branch_taken) begin
-								next_pht[pc_partial][1:0] = 2'b11;
+								next_pht[rt_pc_partial][1:0] = 2'b11;
 							end else begin
-								next_pht[pc_partial][1:0] = 2'b10;
+								next_pht[rt_pc_partial][1:0] = 2'b10;
 							end
 						end
 				endcase
-			end
-
-			if(if_branch & if_cond_branch & !if_return_branch) begin
-				if_prediction = next_pht[pc_partial][1];
-				
 			end
 
 		end
