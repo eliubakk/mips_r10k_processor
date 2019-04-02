f4425369ba348098ec0e3b72d0f3f9834f1317ba
heewookim <heewoo9108@gmail.com>
2019-04-02 17:08:27 -0400

    initializing new testing, mult2.s

--- testbench/pipeline_test.v
+++ testbench/pipeline_test.v
@@ -530,7 +530,7 @@ module testbench;
 
 	task display_stages;
 		begin
-			 if (clock_count == 200) begin
+			 if (clock_count == 25) begin
 				$finish;
 			 end
 			$display("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
