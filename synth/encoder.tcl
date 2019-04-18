b1b506da87a1d42c81bb095cf46ff049f2ad9624
heewookim <heewoo9108@gmail.com>
2019-04-18 17:59:24 -0400

    encoder medium 1.9ns

--- synth/encoder.tcl
+++ synth/encoder.tcl
@@ -18,7 +18,7 @@ set design_name encoder
 ##############################################
 set clock_name clock
 set reset_name reset
-set CLK_PERIOD 2
+set CLK_PERIOD 1.9
 
 
 
@@ -120,7 +120,7 @@ if {  $dc_shell_status != [list] } {
   uniquify
   ungroup -all -flatten
   redirect $chk_file { check_design }
-  compile -map_effort high
+  compile -map_effort medium
   write -hier -format verilog -output $netlist_file $design_name
   write -hier -format ddc -output $ddc_file $design_name
   redirect $rep_file { report_design -nosplit }
